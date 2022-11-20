33f6334b7fa6b8c096792f2a6e80094a500d2032
Дарья Шарипова <darya.sharipova@oldcodex.ru>
2022-11-20 11:07:36 +0500

    выполнено задание 7

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("введите значение х");
+        double x = double.Parse(Console.ReadLine());
+        static double vicheslenie(ref double x)
+        {
+            if(Math.Sin(x)<-0.5)
+            {
+                return Math.Sin(x);
+            }
+            else if ( -0.5 >= Math.Sin(x) & Math.Sin(x) <= 0.5)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        Console.WriteLine(vicheslenie(ref x));
+    }
+}
+
+
+
